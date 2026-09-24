7ce170243334c07a263dcaa6770a293d39dfff33
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-09-24 15:22:00 +0600

    compose project setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,79 +1,54 @@
-import java.util.Properties;
-
 plugins {
     alias(libs.plugins.android.application)
-    id("com.google.gms.google-services")
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "com.github.abrarshakhi.mmap"
+    namespace = "com.abrarshakhi.messman"
     compileSdk {
-        version = release(36)
-    }
-
-    buildFeatures {
-        viewBinding = true
-        buildConfig = true
+        version = release(37)
     }
 
     defaultConfig {
-        applicationId = "com.github.abrarshakhi.mmap"
-        minSdk = 30
-        targetSdk = 36
+        applicationId = "com.abrarshakhi.messman"
+        minSdk = 26
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
-            )
+            optimization {
+                enable = false
+            }
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    implementation(libs.legacy.support.v4)
-    implementation(libs.lifecycle.livedata.ktx)
-    implementation(libs.lifecycle.viewmodel.ktx)
-    implementation(libs.firebase.firestore)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-
-    // firebase
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.analytics)
-    implementation(libs.firebase.auth)
-    implementation(libs.firebase.messaging)
-
-
-    // room
-    implementation(libs.room.runtime)
-    annotationProcessor(libs.room.compiler)
-    testImplementation(libs.room.testing)
-
-    // retrofit
-    implementation(libs.retrofit)
-    implementation(libs.converter.gson)
-
-    // OkHttp
-    implementation(libs.logging.interceptor)
-
-    // crypto
-    implementation(libs.security.crypto)
-
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
--- app/src/main/java/com/github/abrarshakhi/mmap/core/constants/MessMemberRole.java
+++
@@ -1,8 +0,0 @@
-package com.github.abrarshakhi.mmap.core.constants;
-
-public class MessMemberRole {
-    public static String DEFAULT = "DEFAULT";
-    public static String ADMIN = "ADMIN";
-    public static String LEFT = "LEFT";
-    public static String MANAGER = "MANAGER";
-}
